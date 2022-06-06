f4bc69a5075335ac991e590dab9b076c8f8a5905
pawel <finestedm@gmail.com>
2022-06-06 10:38:02 +0200

    added arrays of taken squares by each player and switching of players per round

--- sctipt.js
+++ sctipt.js
@@ -1,7 +1,7 @@
 var grid = document.querySelector('container>grid')
 
 const gameboard = (() => {
-    for (let i = 1; i <= 3; i++) {
+    for (let i = 1; i <= 3; i++) {  // this creates an array of 3x3 with dataset of 1-3 height and width coordinates
         for (let j = 1; j <= 3; j++) {
             let grille = document.createElement('grille');
             grille.setAttribute('class', 'grille')
@@ -11,18 +11,36 @@ const gameboard = (() => {
         }
     }
 
-    const listOfTakenSquares = [];
+    const listOfTakenSquaresByPlayerOne = [];
+    const listOfTakenSquaresByPlayerTwo = [];
 
     const gril = document.querySelectorAll('.grille');
     Array.from(gril).forEach((square) => {
-        square.addEventListener('click', () => {
+        square.addEventListener('click', () => {    // the array gets eventListener reacting to click and then chaning the class to 'taken' which disables it from further action. 
             if ((square.classList.contains('taken')) == 0) {
                 square.classList.add('taken');
-                listOfTakenSquares.push(square);
+                if (game.activePlayer == game.playerOne) {
+                    game.activePlayer = game.playerTwo;         //switches the player
+                    square.classList.add('playerOne')
+                    listOfTakenSquaresByPlayerOne.push(square); // adds the clicked gril to the array for corresponding to the player
+                } else {
+                    game.activePlayer = game.playerOne;
+                    square.classList.add('playerTwo');
+                    listOfTakenSquaresByPlayerTwo.push(square);
+                }
             }
         });
     });
-    return { listOfTakenSquares }; // this is only for diagnostics ;))
+    return { listOfTakenSquaresByPlayerOne, listOfTakenSquaresByPlayerTwo }; // this is only for diagnostics ;))
+})();
+
+const game = (() => {
+    const playerOne = { name: 'Paweł', symbol: "X" };
+    const playerTwo = { name: 'Andrzej', symbol: "O" };
+    let activePlayer = playerOne;
+
+
+    return { playerOne, playerTwo, activePlayer }
 })();
 
 
