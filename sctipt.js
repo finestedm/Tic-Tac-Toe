b14b9f8b5f571773a566414e38606f3ceb1f4121
pawel <finestedm@gmail.com>
2022-06-07 13:40:45 +0200

    added player constructor using factory funtion instead of object constructor and added code to push selected squares to the players list (list in construtor).

--- sctipt.js
+++ sctipt.js
@@ -1,47 +1,67 @@
 var grid = document.querySelector('container>grid')
 
-const gameboard = (() => {
-    for (let i = 1; i <= 3; i++) {  // this creates an array of 3x3 with dataset of 1-3 height and width coordinates
-        for (let j = 1; j <= 3; j++) {
-            let grille = document.createElement('grille');
-            grille.setAttribute('class', 'grille')
-            grille.dataset.width = j;
-            grille.dataset.height = i;
-            grid.append(grille);
-        }
+
+const game = (() => {
+
+    const Player = (name, symbol) => {  // using factory function to create players. 
+        const playerName = name;
+        const playerSymbol = symbol;
+        const listOfTakenSquaresByPlayer = []
+        const addSquareToCurrentPlayer = (square) => {
+            listOfTakenSquaresByPlayer.push(square)
+        };
+        return { addSquareToCurrentPlayer }
     }
 
-    const listOfTakenSquaresByPlayerOne = [];
-    const listOfTakenSquaresByPlayerTwo = [];
+
+
+    // function Player(name, symbol) {   // same results but with object constructor 
+    //     this.name = name;
+    //     this.symbol = symbol;
+    //     this.listOfTakenSquaresByPlayer = []
+    // }
+
+    // Player.prototype.addSquareToCurrentPlayer = function (square) {
+    //     this.listOfTakenSquaresByPlayer.push(square)
+    // };
+
+
+    const playerOne = Player('Paweł', "X"); // name & symbol should be selectable at the beggining of each game.
+    const playerTwo = Player('Andrzej', "O"); // name & symbol should be selectable at the beggining of each game.;
+    let activePlayer = playerOne;
+
+    const gameboard = (() => {
+        for (let i = 1; i <= 3; i++) {  // this creates an array of 3x3 with dataset of 1-3 height and width coordinates
+            for (let j = 1; j <= 3; j++) {
+                let grille = document.createElement('grille');
+                grille.setAttribute('class', 'grille')
+                grille.dataset.width = j;
+                grille.dataset.height = i;
+                grid.append(grille);
+            }
+        }
+    })();
 
     const gril = document.querySelectorAll('.grille');
     Array.from(gril).forEach((square) => {
         square.addEventListener('click', () => {    // the array gets eventListener reacting to click and then chaning the class to 'taken' which disables it from further action. 
             if ((square.classList.contains('taken')) == 0) {
                 square.classList.add('taken');
-                if (game.activePlayer == game.playerOne) {
-                    game.activePlayer = game.playerTwo;         //switches the player
+                if (activePlayer == playerOne) {
+                    playerOne.addSquareToCurrentPlayer(square) //adds selected square to the array of selected squares of activeplayer
+                    activePlayer = playerTwo;         //switches the player
                     square.classList.add('playerOne')
-                    listOfTakenSquaresByPlayerOne.push(square); // adds the clicked gril to the array for corresponding to the player
                 } else {
-                    game.activePlayer = game.playerOne;
-                    square.classList.add('playerTwo');
-                    listOfTakenSquaresByPlayerTwo.push(square);
+                    playerTwo.addSquareToCurrentPlayer(square) //adds selected square to the array of selected squares of activeplayer
+                    activePlayer = playerOne;
+                    square.classList.add('playerTwo')
                 }
             }
         });
     });
-    return { listOfTakenSquaresByPlayerOne, listOfTakenSquaresByPlayerTwo }; // this is only for diagnostics ;))
 })();
 
-const game = (() => {
-    const playerOne = { name: 'Paweł', symbol: "X" };
-    const playerTwo = { name: 'Andrzej', symbol: "O" };
-    let activePlayer = playerOne;
 
-
-    return { playerOne, playerTwo, activePlayer }
-})();
 
 
 
