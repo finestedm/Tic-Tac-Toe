5694ed419ccd2f62b90c11059a8600d913f8ae07
pawel <finestedm@gmail.com>
2022-06-08 21:04:37 +0200

    closed scope for players list of taken squares, checks for win working with .every and .include, and css square color is now based on the symbol a player chooses but it has to be reduces to a small choice so all of them can be stylized. Game logic works!

--- sctipt.js
+++ sctipt.js
@@ -4,22 +4,23 @@ var grid = document.querySelector('container>grid')
 const game = (() => {
 
     const Player = (name, symbol) => {  // using factory function to create players. 
-        const playerName = name;
-        const playerSymbol = symbol;
+        const getName = () => name;
+        const getSymbol = () => symbol;
         const listOfTakenSquaresByPlayer = []
         const addSquareToCurrentPlayer = (square) => {
-            listOfTakenSquaresByPlayer.push(square)
+            listOfTakenSquaresByPlayer.push(parseFloat(square))
         };
-        return { addSquareToCurrentPlayer, listOfTakenSquaresByPlayer }
+        const getListOfTakenSquaresByPlayer = () => (listOfTakenSquaresByPlayer);
+        return { addSquareToCurrentPlayer, getListOfTakenSquaresByPlayer, getName, getSymbol }
     }
 
 
 
     const playerOne = Player('Default', "X"); // name & symbol should be selectable at the beginning of each game.
-    const playerTwo = Player('Andrzej', "O"); // name & symbol should be selectable at the beginning of each game.;
+    const playerTwo = Player('Andrzej', "O"); // name & symbol should be selectable at the beginning of each game.
     let activePlayer = playerOne;
 
-    playerOne.name = window.prompt("Please enter your name", "PlayerOne");
+    playerOne.name = window.prompt("Please enter your name", "PlayerOne");  // this somehow works by overriding the name inside closed Player scope. Or is it?... 
     playerOne.symbol = window.prompt("Please enter your symbol", "X");
 
     // function Player(name, symbol) {   // same results but with object constructor 
@@ -31,16 +32,6 @@ const game = (() => {
     // Player.prototype.addSquareToCurrentPlayer = function (square) {
     //     this.listOfTakenSquaresByPlayer.push(square)
     // };
-
-    const wins = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-        [1, 4, 7],
-        [2, 5, 8],
-        [3, 6, 9],
-        [1, 5, 9],
-        [3, 5, 7]];
 
     const gameboard = (() => {
         for (let i = 1; i <= 9; i++) {  // this creates an array of 3x3 with dataset of 1-9
@@ -61,11 +52,14 @@ const game = (() => {
         [1, 5, 9],
         [3, 5, 7]];
 
-    const checkIfWon = () => {
-        if (playerOne.listOfTakenSquaresByPlayer.length >= 2) {
+    const checkIfWon = (activePlayer) => {
+        console.log(activePlayer.getListOfTakenSquaresByPlayer());
+        if (activePlayer.getListOfTakenSquaresByPlayer().length >= 3) {
             for (let i = 0; i < wins.length; i++) {
-                if ((wins[i]).includes(playerOne.listOfTakenSquaresByPlayer)) {
-                    console.log('test')
+
+                if (wins[i].every(r => (activePlayer.getListOfTakenSquaresByPlayer()).includes(r))) {
+
+                    console.log(`${activePlayer.getName()} wins`);
                 }
             }
         }
@@ -77,17 +71,15 @@ const game = (() => {
         square.addEventListener('click', () => {    // the array gets eventListener reacting to click and then chaning the class to 'taken' which disables it from further action. 
             if ((square.classList.contains('taken')) == 0) {
                 square.classList.add('taken');
-                if (activePlayer == playerOne) {
-                    playerOne.addSquareToCurrentPlayer(square.dataset.location) //adds selected square to the array of selected squares of activeplayer
-                    activePlayer = playerTwo;         //switches the player
-                    square.classList.add('playerOne')
-
+                square.classList.add(`${String(activePlayer.getSymbol())}`);
+                activePlayer.addSquareToCurrentPlayer(square.dataset.location);                    //adds selected square to the array of selected squares of activeplayer
+                checkIfWon(activePlayer);
+                if (activePlayer == playerOne) {  //switches the player
+                    activePlayer = playerTwo;
                 } else {
-                    playerTwo.addSquareToCurrentPlayer(square.dataset.locatio) //adds selected square to the array of selected squares of activeplayer
                     activePlayer = playerOne;
-                    square.classList.add('playerTwo')
                 }
-                checkIfWon();
+
             }
         });
     });
