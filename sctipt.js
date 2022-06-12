77f4861b222e516cfa1eabd2d4bcf20db8583198
pawel <finestedm@gmail.com>
2022-06-13 00:31:24 +0200

    the symbol is now passed from DOM to player object value and the ::after elements shows the chosen symbol

--- sctipt.js
+++ sctipt.js
@@ -1,33 +1,31 @@
 var grid = document.querySelector('container>grid');
 var inputs = document.querySelectorAll('fieldset input');
-
+var buttonInput = document.querySelectorAll(`fieldset input[type='radio']`)
 
 const game = (() => {
 
     const Player = (name, symbol) => {  // using factory function to create players. 
-        const getName = () => name;
-        const getSymbol = () => symbol;
         const listOfTakenSquaresByPlayer = []
         const addSquareToCurrentPlayer = (square) => {
             listOfTakenSquaresByPlayer.push(parseFloat(square))
         };
         const getListOfTakenSquaresByPlayer = () => (listOfTakenSquaresByPlayer);
-        return { addSquareToCurrentPlayer, getListOfTakenSquaresByPlayer, getName, getSymbol }
+        return { addSquareToCurrentPlayer, getListOfTakenSquaresByPlayer, name, symbol }
     }
 
-    inputs.forEach((input) => {
+    buttonInput.forEach((input) => {
         input.addEventListener('click', () => {
             playerOne.name = inputs[0].value;
-            console.log(playerOne.name);
+            playerOne.symbol = input.value;
+
             enableGame();           // enebled game only after symbol has bee chosen
         })
     });
 
     const enableGame = function () {        // enables buttons on grid 
-        console.log(gril)
         Array.from(gril).forEach((square) => {
             square.disabled = false;
-            console.log(square)
+            square.classList.add('enabled')
         })
     }
 
@@ -73,7 +71,7 @@ const game = (() => {
 
                 if (wins[i].every(r => (activePlayer.getListOfTakenSquaresByPlayer()).includes(r))) {
 
-                    console.log(`${activePlayer.getName()} wins`);
+                    console.log(`${activePlayer.name} wins`);
                 }
             }
         }
@@ -85,7 +83,7 @@ const game = (() => {
         square.addEventListener('click', () => {    // the array gets eventListener reacting to click and then chaning the class to 'taken' which disables it from further action. 
             if ((square.classList.contains('taken')) == 0) {
                 square.classList.add('taken');
-                square.classList.add(`${String(activePlayer.getSymbol())}`);
+                square.classList.add(`${String(activePlayer.symbol)}`);
                 activePlayer.addSquareToCurrentPlayer(square.dataset.location);                    //adds selected square to the array of selected squares of activeplayer
                 checkIfWon(activePlayer);
                 if (activePlayer == playerOne) {  //switches the player
