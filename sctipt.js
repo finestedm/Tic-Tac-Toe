6b6241ab9c77a4b48b6508853bc680f463408708
pawel <finestedm@gmail.com>
2022-06-13 22:26:16 +0200

    added ending screen and confetti animation

--- sctipt.js
+++ sctipt.js
@@ -1,6 +1,7 @@
 var grid = document.querySelector('container>grid');
 var inputs = document.querySelectorAll('fieldset input');
-var buttonInput = document.querySelectorAll(`fieldset input[type='radio']`)
+var buttonInput = document.querySelectorAll(`fieldset input[type='radio']`);
+var winnerPanel = document.querySelector('.winner');
 
 const game = (() => {
 
@@ -18,6 +19,7 @@ const game = (() => {
             playerOne.name = inputs[0].value;
             playerOne.symbol = input.value;
             (playerOne.symbol == 'X') ? playerTwo.symbol = 'O' : playerTwo.symbol = 'X';
+            (document.querySelector('fieldset')).remove();
             enableGame();           // enebled game only after symbol has bee chosen
         })
     });
@@ -71,8 +73,10 @@ const game = (() => {
             for (let i = 0; i < wins.length; i++) {
 
                 if (wins[i].every(r => (activePlayer.getListOfTakenSquaresByPlayer()).includes(r))) {
-
-                    console.log(`${activePlayer.name} wins`);
+                    console.log(winnerPanel)
+                    winnerPanel.textContent = `${activePlayer.name} wins!`
+                    winnerPanel.classList.add('visable')
+                    startConfetti();
                 }
             }
         }
@@ -96,6 +100,136 @@ const game = (() => {
             }
         });
     });
+
+    var maxParticleCount = 100; //set max confetti count
+    var particleSpeed = 1; //set the particle animation speed
+    var startConfetti; //call to start confetti animation
+
+    (function () {
+        startConfetti = startConfettiInner;
+        stopConfetti = stopConfettiInner;
+        toggleConfetti = toggleConfettiInner;
+        removeConfetti = removeConfettiInner;
+        var colors = ["DodgerBlue", "OliveDrab", "Gold", "Pink", "SlateBlue", "LightBlue", "Violet", "PaleGreen", "SteelBlue", "SandyBrown", "Chocolate", "Crimson"]
+        var streamingConfetti = false;
+        var animationTimer = null;
+        var particles = [];
+        var waveAngle = 0;
+
+        function resetParticle(particle, width, height) {
+            particle.color = colors[(Math.random() * colors.length) | 0];
+            particle.x = Math.random() * width;
+            particle.y = Math.random() * height - height;
+            particle.diameter = Math.random() * 10 + 5;
+            particle.tilt = Math.random() * 10 - 10;
+            particle.tiltAngleIncrement = Math.random() * 0.07 + 0.05;
+            particle.tiltAngle = 0;
+            return particle;
+        }
+
+        function startConfettiInner() {
+            var width = window.innerWidth;
+            var height = window.innerHeight;
+            window.requestAnimFrame = (function () {
+                return window.requestAnimationFrame ||
+                    window.webkitRequestAnimationFrame ||
+                    window.mozRequestAnimationFrame ||
+                    window.oRequestAnimationFrame ||
+                    window.msRequestAnimationFrame ||
+                    function (callback) {
+                        return window.setTimeout(callback, 16.6666667);
+                    };
+            })();
+            var canvas = document.getElementById("confetti-canvas");
+            if (canvas === null) {
+                canvas = document.createElement("canvas");
+                canvas.setAttribute("id", "confetti-canvas");
+                canvas.setAttribute("style", "display:block;z-index:999999;pointer-events:none");
+                document.body.appendChild(canvas);
+                canvas.width = width;
+                canvas.height = height;
+                window.addEventListener("resize", function () {
+                    canvas.width = window.innerWidth;
+                    canvas.height = window.innerHeight;
+                }, true);
+            }
+            var context = canvas.getContext("2d");
+            while (particles.length < maxParticleCount)
+                particles.push(resetParticle({}, width, height));
+            streamingConfetti = true;
+            if (animationTimer === null) {
+                (function runAnimation() {
+                    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+                    if (particles.length === 0)
+                        animationTimer = null;
+                    else {
+                        updateParticles();
+                        drawParticles(context);
+                        animationTimer = requestAnimFrame(runAnimation);
+                    }
+                })();
+            }
+        }
+
+        function stopConfettiInner() {
+            streamingConfetti = false;
+        }
+
+        function removeConfettiInner() {
+            stopConfetti();
+            particles = [];
+        }
+
+        function toggleConfettiInner() {
+            if (streamingConfetti)
+                stopConfettiInner();
+            else
+                startConfettiInner();
+        }
+
+        function drawParticles(context) {
+            var particle;
+            var x;
+            for (var i = 0; i < particles.length; i++) {
+                particle = particles[i];
+                context.beginPath();
+                context.lineWidth = particle.diameter;
+                context.strokeStyle = particle.color;
+                x = particle.x + particle.tilt;
+                context.moveTo(x + particle.diameter / 2, particle.y);
+                context.lineTo(x, particle.y + particle.tilt + particle.diameter / 2);
+                context.stroke();
+            }
+        }
+
+        function updateParticles() {
+            var width = window.innerWidth;
+            var height = window.innerHeight;
+            var particle;
+            waveAngle += 0.01;
+            for (var i = 0; i < particles.length; i++) {
+                particle = particles[i];
+                if (!streamingConfetti && particle.y < -15)
+                    particle.y = height + 100;
+                else {
+                    particle.tiltAngle += particle.tiltAngleIncrement;
+                    particle.x += Math.sin(waveAngle);
+                    particle.y += (Math.cos(waveAngle) + particle.diameter + particleSpeed) * 0.5;
+                    particle.tilt = Math.sin(particle.tiltAngle) * 15;
+                }
+                if (particle.x > width + 20 || particle.x < -20 || particle.y > height) {
+                    if (streamingConfetti && particles.length <= maxParticleCount)
+                        resetParticle(particle, width, height);
+                    else {
+                        particles.splice(i, 1);
+                        i--;
+                    }
+                }
+            }
+        }
+    })();
+
+
 })();
 
 
